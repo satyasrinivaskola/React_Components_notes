6214074ee77ae7b60611e0f42baf4b622ba3cb07
satyasrinivaskola <satyasrinivaskola@gmail.com>
2025-05-28 16:23:51 +0530

    Added JSX_topic file

---
+++ src/JSX_topic.js
@@ -0,0 +1,11 @@
+import React from 'react'
+/* Function component */
+const JSX_topic = () =>{
+    return(
+       React.createElement("div",null, React.createElement("h1",null,"Hello JSX"))
+        
+       
+    )
+
+}
+export default JSX_topic
